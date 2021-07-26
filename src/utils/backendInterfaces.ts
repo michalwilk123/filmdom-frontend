cde94d07f0314cc399e13703bf09f9c4068431a7
michal <michal.wilk0@yahoo.com>
2021-07-26 15:51:54 +0200

    added interfaces for the backend

---
+++ src/utils/backendInterfaces.ts
@@ -0,0 +1,10 @@
+
+
+export interface MovieListElement{
+    title:string
+    thumbnail:string|null
+    description:string
+    director:string
+    no_of_comments:number // popularity
+    rating:number // best/worst
+}
