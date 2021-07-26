2502a857ce587636bbf755bf21a28941a53f4fc8
michal <michal.wilk0@yahoo.com>
2021-07-26 13:15:59 +0200

    added 404 page

--- src/components/special/PageNotFound.tsx
+++ src/components/special/PageNotFound.tsx
@@ -1,7 +1,12 @@
+import { Flex, Text } from "@chakra-ui/react";
 import React, { ReactElement } from "react";
 
-interface Props {}
-
-export default function PageNotFound({}: Props): ReactElement {
-  return <div>Page not found</div>;
+export default function PageNotFound(): ReactElement {
+  return (
+    <Flex justifyContent="center">
+      <Text fontSize="5xl" as="b">
+        404 Page not found
+      </Text>
+    </Flex>
+  );
 }
