9271441e288b696eb848315648c936cb76849b16
michal <michal.wilk0@yahoo.com>
2021-07-27 13:34:47 +0200

    added utilites for converting rating to star amount

--- src/utils/other.ts
+++ src/utils/other.ts
@@ -1,11 +1,14 @@
+enum STAR_VALUES {
+  FULL = "★",
+  HALF = "⯪", // this icon does not has the same size as the others
+  EMPTY = "☆",
+}
+
 /*
  * Reshaping array of generic typed object into matrix
  * with @cols columns.
  */
-export const arr_reshape = (
-  arr: readonly any[],
-  cols: number
-): Array<any[]> => {
+export const arrReshape = (arr: readonly any[], cols: number): Array<any[]> => {
   let arr_copy = [...arr];
   let res_arr = [];
   let col_arr = [];
@@ -28,4 +31,35 @@ export const arr_reshape = (
   return res_arr;
 };
 
-console.log("djnkjdsa");
+export const calculateStarValue = (rating: number): number | undefined => {
+  if (rating < 0 || rating > 5) {
+    return undefined;
+  }
+  /* we return value closest to
+   * the nearest star value (implementing
+   * the half stars)
+   */
+  return Math.round(rating << 1) / 2;
+};
+
+export const convertRatingToStar = (
+  rating: number,
+  upperBound: number
+): string => {
+  if (!Number.isInteger(upperBound)) {
+    // eslint-disable-next-line no-throw-literal
+    throw `Cannot create rating with non integer value: ${upperBound}`;
+  }
+  // can implement half star if we find proper unicode character for it
+  // let starString = !Number.isInteger(rating) ? STAR_VALUES.HALF : "";
+  let starString = "";
+
+  for (let i = Math.floor(rating); i > 0; i--) {
+    starString = STAR_VALUES.FULL + starString;
+  }
+  for (let i = upperBound - Math.floor(rating); i > 0; i--) {
+    starString = starString + STAR_VALUES.EMPTY;
+  }
+
+  return starString;
+};
