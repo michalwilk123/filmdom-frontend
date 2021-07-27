99ea56ceb0f788e582a42ff71c808a414a35bd71
michal <michal.wilk0@yahoo.com>
2021-07-27 13:49:51 +0200

    made ranking page more responsive

--- src/components/ranking/RankingPage.tsx
+++ src/components/ranking/RankingPage.tsx
@@ -1,23 +1,34 @@
 import { Flex, VStack } from "@chakra-ui/react";
 import React, { ReactElement, useState } from "react";
 import { MovieListElement } from "../../utils/backendInterfaces";
+import { useWindowDimensions } from "../../utils/responsiveUtils";
 import { MovieCard } from "./MovieCard";
 import { MovieFilterFeed } from "./MovieFilterFeed";
 
 export const RankingPage = (): ReactElement => {
   const [movieRanking, setMovieRanking] = useState<MovieListElement[]>([]);
+  const { width } = useWindowDimensions();
 
   return (
     <Flex
       bgColor="white"
       width={{ base: "100vw", md: "90vw", lg: "80vw", xl: "70vw" }}
       rounded="lg"
+      px="5"
     >
-      <MovieFilterFeed rankingStateFunc={setMovieRanking} />
-      <VStack>
-        {movieRanking.map((movie: MovieListElement) => {
-          return <MovieCard movie={movie} />;
-        })}
+      <VStack width="100%">
+        <MovieFilterFeed rankingStateFunc={setMovieRanking} />
+        <VStack px={5} width="100%">
+          {movieRanking.map((movie: MovieListElement) => {
+            return (
+              <MovieCard
+                showthumbnail={true && width > 1200}
+                movie={movie}
+                key={movie.id.toString()}
+              />
+            );
+          })}
+        </VStack>
       </VStack>
     </Flex>
   );
