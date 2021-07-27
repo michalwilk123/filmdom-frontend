08ea554657e7bf8ebbf57a0842e626cda4993c6f
michal <michal.wilk0@yahoo.com>
2021-07-27 13:50:08 +0200

    created enum representing sorting methods

---
+++ src/utils/commonIterfaces.ts
@@ -0,0 +1,9 @@
+export enum MovieSortMethods {
+  POPULARITY_DES = "popularity",
+  POPULARITY_ASC = "popularity (lowest)",
+  BEST = "rating",
+  WORST = "rating (lowest)",
+  NEWEST = "date",
+  OLDEST = "date (oldest)",
+  ALPHABETICAL = "alphabetical",
+}
