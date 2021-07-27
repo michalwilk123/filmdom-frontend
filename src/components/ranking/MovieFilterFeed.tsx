2815eac03d25a680af8dca23782bc7dcf22aecf0
michal <michal.wilk0@yahoo.com>
2021-07-27 13:47:51 +0200

    changed formats of backend response messages

--- src/components/ranking/MovieFilterFeed.tsx
+++ src/components/ranking/MovieFilterFeed.tsx
@@ -1,7 +1,12 @@
-import { Container, Heading } from "@chakra-ui/react";
-import React, { ReactElement } from "react";
+import {
+  Flex,
+  Heading,
+  Select,
+  Spacer,
+} from "@chakra-ui/react";
+import React, { ReactElement, useEffect } from "react";
 import { MovieListElement } from "../../utils/backendInterfaces";
-import { MovieGenreSelector } from "../authorizationRelated/MovieGenreSelector";
+import { MovieSortMethods } from "../../utils/commonIterfaces";
 
 /* NOTE: I chose that we really like the user and sort and filter the data
  * in  backend. I do not know if this approach is correct. We can save up some
@@ -10,29 +15,35 @@ import { MovieGenreSelector } from "../authorizationRelated/MovieGenreSelector";
 
 const testFilmList: MovieListElement[] = [
   {
+    id: 1,
     title: "Django Unchained",
     thumbnail:
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQiM2ek2bgWMbQGeqTnArHy9RoEGS_z_RYUvlDrRxgBCXL0LzUD",
     description: "very cool movie",
     director: "Quentin Tarantino",
-    no_of_comments: 2,
-    rating: 4,
+    noOfComments: 2,
+    avgRating: 4,
+    produceDate: "1999-03-14",
   },
   {
+    id: 10,
     title: "Forrest Gump",
     thumbnail: "https://fwcdn.pl/fpo/09/98/998/7314731.3.jpg",
     description: "nice movie",
     director: "Robert Zemeckis",
-    no_of_comments: 10,
-    rating: 4.2,
+    noOfComments: 10,
+    avgRating: 4.2,
+    produceDate: "2001-02-10",
   },
   {
+    id: 2,
     title: "Lorem Ipsum",
     thumbnail: null,
     description: "w/e movie",
     director: "John Doe",
-    no_of_comments: 0,
-    rating: 0,
+    noOfComments: 0,
+    avgRating: 0,
+    produceDate: "2010-10-01",
   },
 ];
 
@@ -45,17 +56,40 @@ interface Props {
 }
 
 export const MovieFilterFeed = (props: Props): ReactElement => {
-  const handleRankingListRequest = (e: React.SyntheticEvent) => {
-    e.preventDefault();
+  useEffect(() => {
+    handleRankingListRequest();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const handleRankingListRequest = (
+    e: React.SyntheticEvent | undefined = undefined
+  ) => {
+    if (e !== undefined) {
+      e.preventDefault();
+    }
+    const movieList = fetchRanking();
+    props.rankingStateFunc(movieList);
+  };
+
+  const onSortTypeChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    console.log(e.target.value);
     const movieList = fetchRanking();
     props.rankingStateFunc(movieList);
   };
 
   return (
-    <Container py="7">
+    <Flex py="3" px="6" width="inherit">
       <Heading>Ranking</Heading>
-      <form onSubmit={handleRankingListRequest}></form>
-      <MovieGenreSelector py={10} />
-    </Container>
+      <Spacer />
+      <Select
+        _hover={{ cursor: "pointer" }}
+        onChange={onSortTypeChange}
+        width="250px"
+      >
+        {Object.values(MovieSortMethods).map((method) => (
+          <option>{method}</option>
+        ))}
+      </Select>
+    </Flex>
   );
 };
--- src/utils/backendInterfaces.ts
+++ src/utils/backendInterfaces.ts
@@ -1,8 +1,32 @@
 export interface MovieListElement {
+  id: number;
   title: string;
-  thumbnail: string | null;
+  thumbnail: string | null; // url or no photo
   description: string;
   director: string;
-  no_of_comments: number; // popularity
-  rating: number; // best/worst
+  noOfComments: number; // popularity
+  avgRating: number; // best/worst
+  produceDate: string; // newest oldest
+}
+
+export interface MovieComment {
+  creatorId: number;
+  movieId: number;
+  username: string;
+  rating: number;
+  text: string | null;
+  publishDate: string;
+}
+
+export interface UserDtoData {
+  username: string;
+  creationDate: string;
+  thumbnail: string | null; // url or no photo
+  description: string;
+  numberOfComments: number;
+  favouriteGenres: string[];
+}
+
+export interface GenreInterface {
+  name: string;
 }
