a8bbd1de3c006dbd3726280e9280dfc52bdd87f0
michal <michal.wilk0@yahoo.com>
2021-07-30 10:53:06 +0200

    made header responsive

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -3,6 +3,7 @@ import {
   Box,
   Button,
   Center,
+  Flex,
   FormControl,
   Heading,
   HStack,
@@ -11,25 +12,30 @@ import {
   InputRightElement,
   LinkBox,
   Spacer,
+  VStack,
 } from "@chakra-ui/react";
 import React from "react";
 import { Link as ReactLink, useHistory } from "react-router-dom";
+import { useWindowDimensions } from "../utils/responsiveUtils";
 import { CurrentUserStatus } from "./CurrentUserStatus";
 
-export const AppHeader = () => {
-  let history = useHistory();
+const handleSubmit = (e: React.SyntheticEvent) => {
+  e.preventDefault();
+  console.log("siema siema");
+};
 
-  const handleSubmit = (e: React.SyntheticEvent) => {
-    e.preventDefault();
-    console.log("siema siema");
-  };
+interface HeaderVariantsProps{
+}
+
+const AppHeaderDefault = (props:HeaderVariantsProps) => {
+  let history = useHistory();
 
   return (
     <Box bgColor="red.300" height="80px" width="100%">
       <HStack
-        paddingX="7"
+        mx="auto"
         alignContent="center"
-        width={{ base: "100%", xl: "80%" }}
+        width={{ base: "100%", xl: "60%" }}
         paddingY="2"
       >
         <Center>
@@ -48,6 +54,8 @@ export const AppHeader = () => {
           >
             Ranking
           </Button>
+          <Box>
+          </Box>
           <form onSubmit={handleSubmit}>
             <FormControl onSubmit={handleSubmit}>
               <InputGroup>
@@ -55,7 +63,7 @@ export const AppHeader = () => {
                   autoComplete="false"
                   bgColor="white"
                   type="text"
-                  placeholder="szukaj filmu"
+                  placeholder="find movie"
                 />
                 <InputRightElement paddingRight="4">
                   <Button type="submit" h="1.75rem">
@@ -70,4 +78,54 @@ export const AppHeader = () => {
       </HStack>
     </Box>
   );
+}
+
+const AppHeaderSmall = (props:HeaderVariantsProps) => {
+  let history = useHistory();
+
+  return (
+    <Box bgColor="red.300" minH="80px" width="100%">
+      <VStack>
+        <Flex width="100%" px="2" pt="2">
+          <Button
+            colorScheme="red"
+            paddingX="10"
+            onClick={() => history.push("/ranking")}
+          >
+            Ranking
+          </Button>
+          <Spacer/>
+          <CurrentUserStatus/>
+        </Flex>
+        <form onSubmit={handleSubmit} style={{width:"100%", padding:"10px"}}>
+          <FormControl onSubmit={handleSubmit}>
+            <InputGroup >
+              <Input
+                autoComplete="false"
+                bgColor="white"
+                type="text"
+                placeholder="find movie"
+              />
+              <InputRightElement paddingRight="4">
+                <Button type="submit" h="1.75rem">
+                  <SearchIcon />
+                </Button>
+              </InputRightElement>
+            </InputGroup>
+          </FormControl>
+        </form>
+      </VStack>
+    </Box>
+  );
+}
+
+
+export const AppHeader = () => {
+  const {width} = useWindowDimensions();
+
+  if (width > 950){
+    return <AppHeaderDefault/>;
+  } else {
+    return <AppHeaderSmall/>;
+  }
 };
