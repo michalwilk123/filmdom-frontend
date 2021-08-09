1cf011738685ed81822f19a98ac8fdeebd2066a1
michal <michal.wilk0@yahoo.com>
2021-08-09 10:15:24 +0200

    unified and fixed movie data fetching

--- src/components/RandomMoviesFeed.tsx
+++ src/components/RandomMoviesFeed.tsx
@@ -16,39 +16,6 @@ import { MovieSortMethods, ResponseListElement } from "../utils/commonIterfaces"
 import { useWindowDimensions } from "../utils/customHooks";
 import { MovieCard } from "./ranking/MovieCard";
 
-const NUM_OF_MOVIES = 5;
-
-// const testFilmList: MovieListElement[] = [
-//   {
-//     id: 1,
-//     title: "Django Unchained",
-//     thumbnail:
-//       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQiM2ek2bgWMbQGeqTnArHy9RoEGS_z_RYUvlDrRxgBCXL0LzUD",
-//     description: "very cool movie",
-//     director: "Quentin Tarantino",
-//     avgRating: 4,
-//     produceDate: "1999-03-14",
-//   },
-//   {
-//     id: 10,
-//     title: "Forrest Gump",
-//     thumbnail: "https://fwcdn.pl/fpo/09/98/998/7314731.3.jpg",
-//     description: "nice movie",
-//     director: "Robert Zemeckis",
-//     avgRating: 4.2,
-//     produceDate: "2001-02-10",
-//   },
-//   {
-//     id: 2,
-//     title: "Lorem Ipsum",
-//     thumbnail: null,
-//     description: "w/e movie",
-//     director: "John Doe",
-//     avgRating: 0,
-//     produceDate: "2010-10-01",
-//   },
-// ];
-
 interface Props extends StackProps {
   args?: null;
 }
@@ -62,10 +29,10 @@ export const RandomMoviesFeed = (props: Props) => {
   const { width } = useWindowDimensions();
 
   useEffect(() => {
-    setRandomMovieList(fetchRandomMovies());
+    fetchRandomMovies();
   }, []);
 
-  const fetchRandomMovies = (): MovieListElement[] => {
+  const fetchRandomMovies = ():void=> {
     let movieGenres: string[];
     let movieActors: string[];
     let movieList: MovieListElement[] = [];
@@ -78,7 +45,6 @@ export const RandomMoviesFeed = (props: Props) => {
       ])
       .then(
         axios.spread((genreRes, actorRes, movieRes) => {
-          console.log(genreRes.data);
           genreRes.data.sort((a:ResponseListElement,b:ResponseListElement)=>a.id-b.id);
           actorRes.data.sort((a:ResponseListElement,b:ResponseListElement)=>a.id-b.id);
 
@@ -94,9 +60,6 @@ export const RandomMoviesFeed = (props: Props) => {
         })
       )
       .catch((errors) => console.log(errors));
-
-    return movieList;
-    // NOTE: fetching data from api!
   };
 
   return (
--- src/components/ranking/MovieFilterFeed.tsx
+++ src/components/ranking/MovieFilterFeed.tsx
@@ -1,77 +1,12 @@
 import { Flex, Heading, Select, Spacer } from "@chakra-ui/react";
 import axios from "axios";
-import React, { ReactElement, useEffect } from "react";
+import React, { ReactElement, useEffect} from "react";
 import { getMovieActors, getMovieGenres, getMovies } from "../../utils/apiConnector";
 import { dispatchMovie, dispatchMovieGenres, dispatchMovieActors } from "../../utils/apiDispatcher";
 import { MovieListElement } from "../../utils/backendInterfaces";
 import { MovieSortMethods } from "../../utils/commonIterfaces";
 import { sortMethodStringToEnum } from "../../utils/other";
 
-/* NOTE: I chose that we really like the user resources and sort, filter the data
- * in backend. I do not know if this approach is correct. We can save up some
- * cpu time on backend if we leave the data unsorted
- */
-
-// const testFilmList: MovieListElement[] = [
-//   {
-//     id: 1,
-//     title: "Django Unchained",
-//     thumbnail:
-//       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQiM2ek2bgWMbQGeqTnArHy9RoEGS_z_RYUvlDrRxgBCXL0LzUD",
-//     description: "very cool movie",
-//     director: "Quentin Tarantino",
-//     noOfComments: 2,
-//     avgRating: 4,
-//     produceDate: "1999-03-14",
-//   },
-//   {
-//     id: 10,
-//     title: "Forrest Gump",
-//     thumbnail: "https://fwcdn.pl/fpo/09/98/998/7314731.3.jpg",
-//     description: "nice movie",
-//     director: "Robert Zemeckis",
-//     noOfComments: 10,
-//     avgRating: 4.2,
-//     produceDate: "2001-02-10",
-//   },
-//   {
-//     id: 2,
-//     title: "Lorem Ipsum",
-//     thumbnail: null,
-//     description: "w/e movie",
-//     director: "John Doe",
-//     noOfComments: 0,
-//     avgRating: 0,
-//     produceDate: "2010-10-01",
-//   },
-// ];
-
-const fetchRanking = (sort_method:MovieSortMethods): MovieListElement[] => {
-  // NOTE: fetching data from the api!
-  let movieGenres: string[];
-  let movieActors: string[];
-  let movieList: MovieListElement[] = [];
-
-  axios
-    .all([
-      getMovieGenres(),
-      getMovieActors(),
-      getMovies({limit:5, sortMethod: sort_method }),
-    ])
-    .then(
-      axios.spread((genreRes, actorRes, movieRes) => {
-        movieGenres = genreRes.data;
-        movieActors = actorRes.data;
-        movieList = genreRes.data.results.map((x: any) => dispatchMovie(x));
-        movieList
-          .map((e) => dispatchMovieGenres(e, movieGenres))
-          .map((e) => dispatchMovieActors(e, movieActors));
-      })
-    )
-    .catch((errors) => console.log(errors));
-  
-  return movieList;
-};
 
 interface Props {
   rankingStateFunc: React.Dispatch<React.SetStateAction<MovieListElement[]>>;
@@ -79,24 +14,40 @@ interface Props {
 
 export const MovieFilterFeed = (props: Props): ReactElement => {
   useEffect(() => {
-    handleRankingListRequest();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+    fetchRanking(MovieSortMethods.POPULARITY_DES);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const handleRankingListRequest = (
-    e: React.SyntheticEvent | undefined = undefined
-  ) => {
-    if (e !== undefined) {
-      e.preventDefault();
-    }
-    const movieList = fetchRanking(MovieSortMethods.BEST);
+  const fetchRanking = (sort_method:MovieSortMethods): MovieListElement[] => {
+    let movieGenres: string[];
+    let movieActors: string[];
+    let movieList: MovieListElement[] = [];
+    console.log(`method: ${sort_method}`);
 
-    props.rankingStateFunc(movieList);
+    axios
+      .all([
+        getMovieGenres(),
+        getMovieActors(),
+        getMovies({limit:5, sortMethod: sort_method }),
+      ])
+      .then(
+        axios.spread((genreRes, actorRes, movieRes) => {
+          movieGenres = genreRes.data;
+          movieActors = actorRes.data;
+          movieList = movieRes.data.map((x: any) => dispatchMovie(x));
+          movieList
+            .map((e) => dispatchMovieGenres(e, movieGenres))
+            .map((e) => dispatchMovieActors(e, movieActors));
+        props.rankingStateFunc(movieList);
+        })
+      )
+      .catch((errors) => console.log(errors));
+  
+    return movieList;
   };
 
   const onSortTypeChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    const movieList = fetchRanking(sortMethodStringToEnum(e.target.value));
-    props.rankingStateFunc(movieList);
+    fetchRanking(sortMethodStringToEnum(e.target.value));
   };
 
   return (
