b76e78ebbd0a25722b829427714412f2a3ea0e7c
michal <michal.wilk0@yahoo.com>
2021-08-10 08:21:37 +0200

    moved data fetching responsibility

--- src/components/ranking/MovieFilterFeed.tsx
+++ src/components/ranking/MovieFilterFeed.tsx
@@ -1,53 +1,21 @@
 import { Flex, Heading, Select, Spacer } from "@chakra-ui/react";
-import axios from "axios";
-import React, { ReactElement, useEffect} from "react";
-import { getMovieActors, getMovieGenres, getMovies } from "../../utils/apiConnector";
-import { dispatchMovie, dispatchMovieGenres, dispatchMovieActors } from "../../utils/apiDispatcher";
-import { MovieListElement } from "../../utils/backendInterfaces";
+import React, { ReactElement } from "react";
+import { useHistory } from "react-router";
 import { MovieSortMethods } from "../../utils/commonIterfaces";
-import { sortMethodStringToEnum } from "../../utils/other";
-
+import queryString from "query-string";
 
 interface Props {
-  rankingStateFunc: React.Dispatch<React.SetStateAction<MovieListElement[]>>;
+  // rankingStateFunc: React.Dispatch<React.SetStateAction<MovieListElement[]>>;
 }
 
 export const MovieFilterFeed = (props: Props): ReactElement => {
-  useEffect(() => {
-    fetchRanking(MovieSortMethods.POPULARITY_DES);
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  const fetchRanking = (sort_method:MovieSortMethods): MovieListElement[] => {
-    let movieGenres: string[];
-    let movieActors: string[];
-    let movieList: MovieListElement[] = [];
-    console.log(`method: ${sort_method}`);
-
-    axios
-      .all([
-        getMovieGenres(),
-        getMovieActors(),
-        getMovies({limit:5, sortMethod: sort_method }),
-      ])
-      .then(
-        axios.spread((genreRes, actorRes, movieRes) => {
-          movieGenres = genreRes.data;
-          movieActors = actorRes.data;
-          movieList = movieRes.data.map((x: any) => dispatchMovie(x));
-          movieList
-            .map((e) => dispatchMovieGenres(e, movieGenres))
-            .map((e) => dispatchMovieActors(e, movieActors));
-        props.rankingStateFunc(movieList);
-        })
-      )
-      .catch((errors) => console.log(errors));
-  
-    return movieList;
-  };
+  let history = useHistory();
 
   const onSortTypeChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    fetchRanking(sortMethodStringToEnum(e.target.value));
+    const args = {
+      sort_method: e.target.value,
+    };
+    history.push(`/ranking/${queryString.stringify(args)}`);
   };
 
   return (
