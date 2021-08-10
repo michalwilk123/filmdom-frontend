28db6a2ddc3188331c158b25bafed275ae39fc8e
michal <michal.wilk0@yahoo.com>
2021-08-10 09:11:24 +0200

    fixed bug with updating query params and removed random sort method

--- src/components/ranking/MovieFilterFeed.tsx
+++ src/components/ranking/MovieFilterFeed.tsx
@@ -1,6 +1,6 @@
 import { Flex, Heading, Select, Spacer } from "@chakra-ui/react";
 import React, { ReactElement } from "react";
-import { useHistory } from "react-router";
+import { useHistory, useParams } from "react-router";
 import { MovieSortMethods } from "../../utils/commonIterfaces";
 import queryString from "query-string";
 
@@ -10,12 +10,20 @@ interface Props {
 
 export const MovieFilterFeed = (props: Props): ReactElement => {
   let history = useHistory();
+  const { args } = useParams<{ args: string | undefined }>();
 
   const onSortTypeChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    const args = {
-      sort_method: e.target.value,
-    };
-    history.push(`/ranking/${queryString.stringify(args)}`);
+    let queryArgs;
+    if (args){
+      queryArgs = {
+        ...queryString.parse(args), sort_method: e.target.value 
+      };
+    } else {
+      queryArgs = {
+        sort_method: e.target.value 
+      };
+    }
+    history.push(`/ranking/${queryString.stringify(queryArgs)}`);
   };
 
   return (
@@ -27,7 +35,7 @@ export const MovieFilterFeed = (props: Props): ReactElement => {
         onChange={onSortTypeChange}
         width="250px"
       >
-        {Object.values(MovieSortMethods).map((method) => (
+        {Object.values(MovieSortMethods).filter((m) => m !== "random").map((method) => (
           <option key={method}>{method}</option>
         ))}
       </Select>
